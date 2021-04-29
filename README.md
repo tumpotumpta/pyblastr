750bfc66352bc66816ba9cec3e56e82abf391045
tumpotumpta <rG/6=MkYSvYL`j(a5@r2>
2021-04-29 15:05:18 +0200

    added README and made more changes to map plot

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pyblastr
+
+This fork of the Pyblastr repository gets rid of everything that is needed to build the website and focuses on extracting the underlying data aswell as creating a couple of simple summary statistic and a map plot.
+
+![Sampling locations of Pyricularia strains compiled in the Pyblastr database plotted on a world map](map.png)
--- summariseData.R
+++ summariseData.R
@@ -35,23 +35,24 @@ p_lin <- wb_all %>%
 # import country borders
 world <- map_data(map = "world",
                   region = ".",
-                  exact = FALSE,
                   # projection = "cylindrical",
-                  boundary = FALSE,
                   wrap = c(-180, 180),
                   ylim = c(-60, 90)) # 
 
 # plot strains on map
 map <- ggplot(world, aes(x = long, y = lat), group = group) +
-  geom_path(aes(group = group), size = 0.2) +
-  # geom_polygon(aes(group = group)) +
-  geom_point(data = wb_all, aes(x = lon, y = lat, fill = wheat), size = 1.5, shape = "circle filled") +
+  geom_path(aes(group = group), size = 0.2, colour = "#331a00") +
+  geom_polygon(aes(group = group), fill = "#f9f7f2") +
+  geom_point(data = wb_all,
+             aes(x = lon, y = lat, fill = wheat),
+             size = 1.5,
+             shape = "circle filled") +
   scale_y_continuous(breaks = (-2:2) * 30) +
-  scale_x_continuous(breaks = (-4:4) * 45) +
+  scale_x_continuous(breaks = (-4:4) * 45, expand = c(0, 0)) +
   scale_fill_viridis_d(option = "D", begin = 0.35, end = 0.7) +
-  labs(title = "Sampling locations of Pyricularia strains compiled in Pyblastr") +
+  labs(title = "Sampling locations of Pyricularia strains compiled in the Pyblastr database") +
   theme_void() + # void, light, minimalist
   theme(legend.title = element_blank(),
-        legend.position = "top")
-# map
-ggsave("map.png", plot = map, width = 20, height = 10, units = "cm", dpi = 300)
+        legend.position = "top",
+        panel.background = element_rect(fill = "#d5e7ea"))
+ggsave("map.png", plot = map, width = 20, height = 11, units = "cm", dpi = 300)
